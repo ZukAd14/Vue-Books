0f92be6470a0527c1d32b301a9c27fa2c41288ff
Adam Żukowski <adam.zukowski471@gmail.com>
2024-04-09 14:12:56 +0200

    Init project with base code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <!-- heading -->
+    <header>
+      <h1>Books<span>.app</span></h1>
+    </header>
+
+    <!-- books list -->
+    <ul>
+      <li
+        :key="index"
+        v-for="(book, index) in books">
+          {{ book.title }}, {{ book.price }}$
+          <button @click="removeBook(index)">Remove</button>
+      </li>
+    </ul>
+
+    <!-- no books message -->
+    <p v-if="!books.length">No books...</p>
+
+    <!-- add book form -->
+    <form @submit.prevent="handleSubmit">
+      <label>
+        Title:
+        <input v-model="form.title" type="text" name="title">
+      </label>
+      <label>
+        price:
+        <input v-model="form.price" type="number" name="price">
+      </label>
+      <button>Add book</button>
+    </form>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    books: [
+      {
+        title: 'The Catcher in the Rye',
+        price: 20
+      },
+      {
+        title: 'Of Mice and Men',
+        price: 18
+      }
+    ],
+    form: {
+      title: '',
+      price: 0
+    }
+  }),
+  methods: {
+    removeBook (index) {
+      this.books.splice(index, 1)
+    },
+    handleSubmit () {
+      this.books.push({ ...this.form })
+    }
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
